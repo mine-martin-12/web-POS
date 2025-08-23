97aa74d3e3207a9bf71cd972e93dd42aec0fa96e
mine-martin-12 <martinmine22@gmail.com>
2025-08-23 20:00:50 +0300

    Feature: Type checking and linting during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { checker } from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,8 +11,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // Type checking and linting during development
+    mode === "development" &&
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+      }),
   ].filter(Boolean),
   resolve: {
     alias: {
